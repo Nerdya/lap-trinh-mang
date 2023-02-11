66bc901b9928fc3b4ab60fbe72dac59fd5b04f21
cuongnk <cuongnk@thinhtientech.com>
2023-02-11 08:54:15 +0700

    Lab2

---
+++ src/com/Lab2/Bai2.java
@@ -0,0 +1,8 @@
+package com.Lab2;
+
+public class Bai2 {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/Lab2/Product.java
@@ -0,0 +1,60 @@
+package com.Lab2;
+
+import com.SharedMethods;
+
+public class Product {
+    private String tenHangHoa;
+    private String nhaSanXuat;
+    private float giaBan;
+
+    public Product(String tenHangHoa, String nhaSanXuat, float giaBan) {
+        this.tenHangHoa = tenHangHoa;
+        this.nhaSanXuat = nhaSanXuat;
+        this.giaBan = giaBan;
+    }
+
+    public Product() {
+        this.tenHangHoa = "";
+        this.nhaSanXuat = "";
+        this.giaBan = 0;
+    }
+
+    public String getTenHangHoa() {
+        return tenHangHoa;
+    }
+
+    public void setTenHangHoa(String tenHangHoa) {
+        this.tenHangHoa = tenHangHoa;
+    }
+
+    public String getNhaSanXuat() {
+        return nhaSanXuat;
+    }
+
+    public void setNhaSanXuat(String nhaSanXuat) {
+        this.nhaSanXuat = nhaSanXuat;
+    }
+
+    public float getGiaBan() {
+        return giaBan;
+    }
+
+    public void setGiaBan(float giaBan) {
+        this.giaBan = giaBan;
+    }
+
+    static SharedMethods sm = new SharedMethods();
+
+    void input() {
+        this.tenHangHoa = sm.inputString("tên hàng hóa");
+        sm.sc.nextLine();
+        this.nhaSanXuat = sm.inputString("nhà sản xuất");
+        this.giaBan = sm.inputFloat("giá bán", "whole");
+    }
+
+    void print() {
+        System.out.println("Tên hàng hóa: " + this.tenHangHoa);
+        System.out.println("Nhà sản xuất: " + this.nhaSanXuat);
+        System.out.println("Giá bán: " + this.giaBan);
+    }
+}
