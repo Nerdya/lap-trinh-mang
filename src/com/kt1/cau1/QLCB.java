6580917980eeb5858aee080be234568234590848
Nguyen Kim Cuong <cuongnk@thinhtientech.com>
2023-04-22 08:23:36 +0700

    kt1

--- src/com/kt1/cau1/QLCB.java
+++ src/com/kt1/cau1/QLCB.java
@@ -9,10 +9,12 @@ public class QLCB {
     int choice;
     do {
       System.out.print(
+          "---------------------Menu--------------------\n" +
           "1. Nhập thông tin mới cho cán bộ\n" +
           "2. Tìm kiếm theo họ tên\n" +
           "3. Hiển thị thông tin về danh sách các cán bộ\n" +
           "4. Thoát khỏi chương trình\n" +
+          "---------------------------------------------\n" +
           "Nhập lựa chọn: "
       );
       choice = Integer.parseInt(sc.nextLine());
@@ -46,8 +48,30 @@ public class QLCB {
           }
           break;
         case 2:
+          String hoTen;
+          boolean found = false;
+          CanBo canBoCanTim = new CanBo();
+          System.out.print("Nhập họ tên cần tìm kiếm: ");
+          hoTen = sc.nextLine();
+          for (CanBo canBo : canBoList) {
+            if (canBo.getHoTen().equals(hoTen)) {
+              found = true;
+              canBoCanTim = canBo;
+            }
+          }
+          if (found) {
+            System.out.println("Thông tin cán bộ cần tìm: ");
+            canBoCanTim.hienThi();
+          } else {
+            System.out.println("Không tìm thấy cán bộ với họ tên " + hoTen);
+          }
           break;
         case 3:
+          System.out.println("Thông tin danh sách các cán bộ: ");
+          for (CanBo canBo : canBoList) {
+            canBo.hienThi();
+            System.out.println();
+          }
           break;
         case 4:
           System.out.println("Thoát chương trình...");
