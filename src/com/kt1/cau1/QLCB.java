7085820a4ffa69e3bd7110df86e91764c2308ba5
Nguyen Kim Cuong <cuongnk@thinhtientech.com>
2023-04-22 08:15:48 +0700

    kt1

--- src/com/kt1/cau1/QLCB.java
+++ src/com/kt1/cau1/QLCB.java
@@ -1,8 +1,6 @@
 package com.kt1.cau1;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class QLCB {
   public static void main(String[] args) {
@@ -20,13 +18,39 @@ public class QLCB {
       choice = Integer.parseInt(sc.nextLine());
       switch (choice) {
         case 1:
-
+          int position;
+          System.out.print("Thêm (1. Công nhân, 2. Kỹ sư, 3. Nhân viên) ? ");
+          position = Integer.parseInt(sc.nextLine());
+          switch (position) {
+            case 1:
+              System.out.println("Thêm công nhân mới.");
+              CongNhan congNhan = new CongNhan();
+              congNhan.nhap();
+              canBoList.add(congNhan);
+              break;
+            case 2:
+              System.out.println("Thêm kỹ sư mới.");
+              KySu kySu = new KySu();
+              kySu.nhap();
+              canBoList.add(kySu);
+              break;
+            case 3:
+              System.out.println("Thêm nhân viên mới.");
+              NhanVien nhanVien = new NhanVien();
+              nhanVien.nhap();
+              canBoList.add(nhanVien);
+              break;
+            default:
+              System.out.println("Vị trí không hợp lệ.");
+              break;
+          }
           break;
         case 2:
           break;
         case 3:
           break;
         case 4:
+          System.out.println("Thoát chương trình...");
           break;
         default:
           System.out.println("Lựa chọn không hợp lệ.");
