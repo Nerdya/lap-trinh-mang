fc3d1edcd2fb51d4d3fcd3b1d62da9dff58d2595
Nguyen Kim Cuong <cuongnk@thinhtientech.com>
2023-05-06 08:24:50 +0700

    bt06052023

---
+++ src/com/bt06052023/Cau5.java
@@ -0,0 +1,12 @@
+package com.bt06052023;
+
+/**
+ * @author cuongnk
+ * @since 24/04/2023
+ */
+public class Cau5 {
+  public static void main(String[] args) {
+    StudentManagerFrame studentManagerFrame = new StudentManagerFrame();
+    studentManagerFrame.setVisible(true);
+  }
+}
